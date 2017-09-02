1dd4363f5f43e73f0810f1907e87ddbcc53c12f3
joshua morris <morrisons233@gmail.com>
2017-09-02 22:57:51 +0100

    Old Code. New Solution. Could not open old one. Added DB Table list button and system buttons.

---
+++ SkopesStockCont/ResultsList.xaml.cs
@@ -0,0 +1,32 @@
+﻿using SkopesStockCont.buttonFunc;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SkopesStockCont
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class ResultList : Window
+    {
+        public ResultList()
+        {
+            InitializeComponent();
+            var results = GetDBTables.Get();
+
+            ResultListBox.ItemsSource = results;
+        }
+    }
+}
---
+++ SkopesStockCont/buttonFunc/GetDBTables.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SkopesStockCont.buttonFunc
+{
+    public static class GetDBTables
+    {
+        static public List<String> Get()
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "work-1.database.windows.net";
+                builder.UserID = "jm2000";
+                builder.Password = "Jbmerhei!";
+
+                using (SqlConnection conn = new SqlConnection(builder.ConnectionString))
+                {
+                    conn.Open();
+
+                    String sql = "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'";
+
+                    using (SqlCommand command = new SqlCommand(sql, conn))
+                    {
+                        
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            List<String> results = new List<String>();
+                            while (reader.Read())
+                            {
+                                results.Add(reader.GetString(2));
+                            }
+                            return results;
+                        }
+                    }
+
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+    }
+}
